28c12c5fcfb3adbe73a05082bab6f49f06194372
Humberto Hernandez <h5hernan@ucsd.edu>
2018-06-04 17:12:02 -0700

    Update nonempty_rows.py

--- Python Scripts/nonempty_rows.py
+++ Python Scripts/nonempty_rows.py
@@ -1,25 +1,24 @@
 import os
 import pandas as pd
-
+#os.getcwd() returns a string for the current directory.
+#os.lisdir() returns a list of all the current items in that directory.
+#print os.getcwd()
 main = os.getcwd()
-path = os.getcwd() + '\UserHisNonEmptyRows'
-new_path = os.getcwd() + '\UserHistory2011'
-
+path = os.getcwd() + '\User History'
+#print os.listdir(path)
+new_path = os.getcwd() + '\UserHisNonEmptyRows'
 os.chdir(path)
 
-assert os.path.exists(path), "Warning! Directory must contain 'UserHisNonEmpty' folder!' 
+assert os.path.exists(path), "Warning! Directory must contain 'User History' folder!' 
 
 if not os.path.exists(new_path):
     os.makedirs(new_path)
-#UTC Time
-begin_year = 1293840000 #Jan 1, 2011
-end_year =   1325376000 #Jan 1, 2012
-    
+
 for filename in os.listdir(path):
-    user_data = pd.read_csv(filename, delimiter = '\t', index_col = '0')
-    user_2011 = user_data.loc[begin_year:end_year]
-    user_2011.reset_index(inplace=True) #Resets the row names to integer names again.
-    new_filename = filename[:-4] + '_2011.csv'
-    user_2011.to_csv(new_path + '\\'+ new_filename, sep = '\t', index = False)
+    history = pd.read_csv(filename, delimiter = '\t', header = None)
+    history_relevant = history.drop([2,3], axis = 1)
+    new_history = history_relevant.dropna()
+    new_filename = filename[:-4] + '.csv'
+    new_history.to_csv(new_path + '\\'+ new_filename, sep = '\t', index = False)
     
 os.chdir(main)
