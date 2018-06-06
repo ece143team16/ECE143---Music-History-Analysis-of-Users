9e86596d7c3f88c3cef8dd0d7dd188282bff5977
Humberto Hernandez <h5hernan@ucsd.edu>
2018-06-05 19:30:10 -0700

    Update genre_listener_country.py

--- Python Scripts/genre_listener_country.py
+++ Python Scripts/genre_listener_country.py
@@ -5,6 +5,9 @@ import os
 import gzip
 import tarfile
 
+assert os.path.isfile('MBID_dictionary.p'), 'Warning! MBID_dictionary.p must be in current directory!'
+assert os.path.isfile('demographics.p'), 'Warning! demographics.p must be in current directory!'
+
 demographics = pd.read_pickle('demographics.p')
 MBID_dictionary = pickle.load( open( "MBID_dictionary.p", "rb" ) )
 
@@ -13,7 +16,7 @@ def user_country_genre(filename,genres):
     Looks inside a the .tar file of the MLHD data set. Unzips the folder and 
     looks through the list of folders. It will then check the users for the genres
     specified in the the list 'genres' and identify the country. We do this for all
-    the users in the .tar file.
+    the users in the .tar file. Does so for the year 2011.
     
     Parameter: filename
     Type: str
@@ -22,6 +25,35 @@ def user_country_genre(filename,genres):
     Parameter: genres
     Type: list
     e.g. genres = ['pop','hiphop','rock']
+    
+    Returns: dictionary containing sub-dictionaries for each country, where each
+            item in this sub dictionary contains the data for 
+    
+    e.g.
+    >>> user_country_genre('MLHD_000.tar',['rock','pop','hiphop','jazz','blue','kpop','rap','disco','edm'])
+    {'BR': {'blue': 0, 'kpop': 2, 'jazz': 46, 'pop': 68, 'disco': 45, 
+            'hiphop': 25, 'rap': 53, 'rock': 72, 'edm': 0}, 
+    'DE': {'blue': 0, 'kpop': 4, 'jazz': 57, 'pop': 73, 'disco': 45, 
+           'hiphop': 47, 'rap': 64, 'rock': 75, 'edm': 14},
+    'NL': {'blue': 0, 'kpop': 0, 'jazz': 18, 'pop': 20, 'disco': 17, 
+           'hiphop': 18, 'rap': 20, 'rock': 20, 'edm': 6},
+    'UK': {'blue': 0, 'kpop': 2, 'jazz': 64, 'pop': 70, 'disco': 58,
+           'hiphop': 53, 'rap': 59, 'rock': 71, 'edm': 6},
+    'US': {'blue': 0, 'kpop': 8, 'jazz': 145, 'pop': 165, 'disco': 128,
+           'hiphop': 129, 'rap': 147, 'rock': 168, 'edm': 11}}
+    
+    First it looks for the file in the current directory. Then it opens it into
+    a new directory in the current directory. It then goes inside that directory
+    and we iterate through all the files in the new directory. We open a user's
+    data and we first process it. First it removes empty rows, which are sometimes
+    created when there is user activity, but there are no songs, or artists. Then,
+    we take only the data that is specific to the year 2011. Before, we proceed,
+    we make sure that the user is found in the demopraphics pandas table. If they are
+    not found in that table, then we cannot analyze them for demographics data
+    and we skip them. If they are found, then proceed to check their data, line
+    by line for the genres we are looking for. After we have gone through all their
+    data, we include them in the counting for how many users listened to certain
+    genres for a given country.
     
     Assertions:
         - filename must be str
